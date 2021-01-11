1b9339d02cb2c2b70a5afef4cfc9009a489e601e
dattrantienht <74689952+dattrantienht@users.noreply.github.com>
2021-01-11 22:23:20 +0700

    .

---
+++ cameracontroler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroler : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+    public float zoomspeed = 4f;
+    public float minzoom = 1.9f;
+    public float maxzoom = 6f;
+    public float yawSpeed = 100f;
+    
+    
+    public float pitch = 2f;
+    private float currentZoom = 5f;
+    private float currentYaw = 0f;
+    void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel")*zoomspeed;
+        currentZoom = Mathf.Clamp(currentZoom,minzoom,maxzoom);
+
+        currentYaw += Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+        
+    }
+    void LateUpdate()
+    {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position,Vector3.up,currentYaw);
+    }
+}
---
+++ playermotor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class playermotor : MonoBehaviour
+{
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void movetopoint(Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+}
