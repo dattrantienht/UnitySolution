2dba4dd2a355804e7586b80e6ed57199e509c1e8
dattrantienht <74689952+dattrantienht@users.noreply.github.com>
2021-01-13 20:15:38 +0700

    .

---
+++ checkmatdat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkmatdat : MonoBehaviour
+{
+    public Transform vatthe;
+    public bool isGrounded;
+    public float distance = 2.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        Vector3 dir = new Vector3(0f, -1f, 0f);
+
+        if (Physics.Raycast(vatthe.position, dir, out hit, distance))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        Debug.Log("co tren mat dat: " + isGrounded);
+        Debug.DrawLine(vatthe.position,hit.point,Color.red);
+    }
+}
---
+++ gravity_and_jump.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravity_and_jump : MonoBehaviour
+{
+    public GameObject nhanvat;
+    public CharacterController controller;
+    public float lucHut = -9.8f; //phai la so am
+    Vector3 playerVelocity;
+
+    public float lucNhay = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool dangdung = nhanvat.GetComponent<checkmatdat>().isGrounded;
+
+        if (dangdung && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        if (Input.GetButtonDown("Jump") && dangdung)
+        {
+            playerVelocity.y += Mathf.Sqrt(lucNhay * -3.0f * lucHut);
+        }
+
+        playerVelocity.y += lucHut * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
