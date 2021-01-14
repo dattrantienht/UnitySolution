cfe7e3a6a29cb142d69387a81c4e2c993b81d634
dattrantienht <74689952+dattrantienht@users.noreply.github.com>
2021-01-14 09:48:46 +0700

    .

--- gravity_and_jump.cs
+++ gravity_and_jump.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿//jump mot cach kho hieu
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
---
+++ jump.cs
@@ -0,0 +1,37 @@
+﻿//jump mot cach de hieu
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    public GameObject raycheck;
+    public CharacterController controller;
+    public float lucHut = 9.8f;
+    public float lucNhay = 3f;
+    Vector3 playerVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool dangdung = raycheck.GetComponent<checkmatdat>().isGrounded;
+
+        if (dangdung)
+        {
+            playerVelocity.y = -lucHut * Time.deltaTime;
+            if (Input.GetButtonDown("Jump"))
+            {
+                playerVelocity.y = lucNhay;
+            }
+        }
+        else
+        {
+            playerVelocity.y -= lucHut * Time.deltaTime;
+        }
+
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log("vector nhay la: " + playerVelocity );
+
+    }
+}
