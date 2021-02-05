70840f9c77b6218fd92a9d5896774562fbc65da1
dattrantienht <74689952+dattrantienht@users.noreply.github.com>
2021-02-06 04:03:15 +0700

    .

---
+++ Polygon Project/Audio/playerAudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAudio : MonoBehaviour
+{
+    //AudioSource AudioSource;
+    //public AudioClip tiengbuocchan;
+    Animator playerAnim;
+    private void Awake()
+    {
+        //AudioSource = GetComponent<AudioSource>();
+        playerAnim = GetComponent<Animator>();
+    }
+    void buocChan()
+    {
+        if(playerAnim.GetFloat("speedpercent")>0.1f && playerAnim.GetFloat("speedpercent")<0.6f)
+        {
+            Debug.Log("ham buoc chan dc goi");
+            //AudioSource.PlayOneShot(tiengbuocchan, 0.7f);
+
+            FindObjectOfType<AudioManager>().Play("tieng_buoc_chan");
+        }
+    }
+
+    void chay()
+    {
+        if (playerAnim.GetFloat("speedpercent") >= 0.6f)
+        {
+            Debug.Log("ham chay dc goi");
+            FindObjectOfType<AudioManager>().Play("tieng_chay");
+        }
+    }
+}
